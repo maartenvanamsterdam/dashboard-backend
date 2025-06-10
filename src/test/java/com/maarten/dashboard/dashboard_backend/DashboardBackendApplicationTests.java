a916a2b1e012af069221dbf4aecfaa7d7d32d99f
Maarten van Amsterdam <mk.vanamsterdam@gmail.com>
2025-06-10 21:30:01 +0200

    Initial Spring Boot project

---
+++ src/test/java/com/maarten/dashboard/dashboard_backend/DashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maarten.dashboard.dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

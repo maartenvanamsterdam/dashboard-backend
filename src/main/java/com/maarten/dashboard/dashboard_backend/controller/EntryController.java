3957efcafeec4436fffd55cc0d1bcb6b40449469
Maarten van Amsterdam <mk.vanamsterdam@gmail.com>
2025-06-11 02:15:59 +0200

    feat: add EntryForm component and backend integration

--- src/main/java/com/maarten/dashboard/dashboard_backend/controller/EntryController.java
+++ src/main/java/com/maarten/dashboard/dashboard_backend/controller/EntryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/entries")
 public class EntryController {
